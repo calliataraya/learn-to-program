ca5882c38998a7ea897aa60b12cdf3e5813a83a5
Callia Taraya <calliataraya@Roberts-MacBook-Pro.local>
2025-06-26 17:23:35 -0500

    third practice

---
+++ hours_in_a_year.rb
@@ -0,0 +1,7 @@
+# Prompt: Have the program output the number of hours in a year.
+# Goal: Practice numeric math and storing intermediate results in variables (days_in_year,hours_in_day).
+
+days_year = 365
+hours_day = 24
+hours_year = days_year * hours_day
+puts 'there are ' + hours_year.to_s + ' hours in a year'
