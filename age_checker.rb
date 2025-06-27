64805601b518ec1c6ff82d116bb77e13466895a1
Callia Taraya <calliataraya@Roberts-MacBook-Pro.local>
2025-06-27 10:30:40 -0500

    check and identify age

---
+++ age_checker.rb
@@ -0,0 +1,13 @@
+# Prompt: Ask the user how old they are. If they're under 13, say they're a kid. If under 20, say teen. if under 65, say adult. Otherwise say senior.
+
+puts 'How old are you?'
+age = gets.chomp.to_i
+if age < 13
+  puts 'you are a kid'
+elsif age < 20
+  puts 'you are a teen'
+elsif age < 65
+  puts 'you are an adult'
+else
+  puts 'you are a senior'
+end
