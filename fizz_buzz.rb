177e6c0684f357bcab38f4e2af13ff4d5674354a
Callia Taraya <calliataraya@Callias-MacBook-Pro-2.local>
2025-07-14 19:57:07 -0700

    learning how to use modulos with fizz buzz

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,11 +1,3 @@
-# Write a program that prints the numbers from 1 to 100.
-
-#  But for multiples of 3, print "Fizz" instead of the number.
-
-#  For multiples of 5, print "Buzz".
-
-#  For numbers which are multiples of both 3 and 5, print "FizzBuzz".
- 
 # Sample Output (partial)
 # 1
 
@@ -35,4 +27,27 @@
 
 #  14
 
-#  FizzBuzz both multiples of 3 and 5
+#  FizzBuzz both multiples of 3 and 5 
+
+# Write a program that prints the numbers from 1 to 100.
+
+#  But for multiples of 3, print "Fizz" instead of the number.
+
+#  For multiples of 5, print "Buzz".
+
+#  For numbers which are multiples of both 3 and 5, print "FizzBuzz".
+ 
+x = 1
+
+while x <= 100
+    if x % 3 == 0 && x % 5 == 0
+        puts 'fizzbuzz'
+    elsif x % 5 == 0
+        puts 'buzz'
+    elsif x % 3 == 0
+        puts 'fizz'
+    else
+        puts x
+    end
+    x = x + 1
+end
