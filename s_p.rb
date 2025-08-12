e2042b650a38d09ee610512ec6aea21a894ae619
Callia Taraya <calliataraya@Callias-MacBook-Pro-2.local>
2025-08-12 11:46:37 -0700

    wp

--- s_p.rb
+++ s_p.rb
@@ -3,8 +3,9 @@ puts 'You are going to talk to Eric Cartman. (from the show southpark) coolio?'
 dialouge = gets.chomp.downcase
 
 def cartman_intro(conversation)
-    cartman_responses = ["Hey whatsup?", "nice"]
-    puts cartman_responses
+    puts "Hey whatsup?"
+    dialouge = gets.chomp.downcase
+    puts "nice"
 end
 def nice_cartman(conversation)
     nice_cartman_responses = ["nice dude", "so whats up now?"]
@@ -17,33 +18,34 @@ end
 def kenny(conversation)
     kenny_responses = ["Ghrmph... mmm...","...mmMh...MMmhHHm..","..."]
     puts kenny_responses.sample
+    puts kenny_responses.sample
+    puts "Ghrmph... bye.. mmm..."
 end
 
-2.times do
-    cartman_intro(conversation)
-    dialouge = gets.chomp.downcase
+count = 0
+loop do
+    cartman_intro(dialouge)
+    count += 1
+    break if count == 1
 end
 
 puts "have you met my BFF Kenny?"
-dialouge = gets.chomp.downcase
+dialouge = gets.chomp.downcase.to_s
 
-if dialouge.include?("no", "maybe", "nah", "nope")
+if dialouge.include?('no')
     puts "OH! KENNY!!!!!!"
     kenny(dialouge)
-elsif dialouge.include?("yes", "yea", "yeah")
+elsif dialouge.include?('yea')
     nice_cartman(dialouge)
 else
     puts "what?"
     puts "have you met my BFF Kenny?"
     dialouge = gets.chomp.downcase
 end
-
-if dialouge.include?("mom", "fat", "momma", "fatass", "wide", "butterballs")
+if dialouge.include?["mom", "fat", "momma", "fatass", "wide", "butterballs", "kenny"]
     mean_cartman(dialouge)
-elsif dialouge.include?("kenny")
-    kenny(dialouge)
-else dialouge.include?("overtime")
+elsif dialouge.include?["bye"]
+    puts "Screw you guys, I/'m leaving!"
+else
    nice_cartman_responses(dialouge)
 end
-
-puts "Screw you guys, I/'m leaving!"
