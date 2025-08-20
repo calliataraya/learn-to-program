83a4ac957c223da8d25cff2a218b78013a2c3bc6
Callia Taraya <calliataraya@Callias-MacBook-Pro-2.local>
2025-08-20 13:42:47 -0700

    solve temperature commenter problem

--- practice_flow_control_and_methods/temperature_commenter.rb
+++ practice_flow_control_and_methods/temperature_commenter.rb
@@ -3,3 +3,16 @@
 # < 50 → “Cold”
 # 50–80 → “Warm”
 # > 80 → “Hot”
+
+puts 'give me a temperature in fahrenheit (use numbers only)'
+temperature = gets.chomp.to_f
+
+if temperature < 50
+  puts 'cold'
+elsif temperature >= 50 && temperature <= 80
+  puts 'warm'
+elsif temperature > 80
+  puts 'hot'
+else
+  puts '?'
+end
