92f6bd232975520d74b4392622005b3a4e73660f
Callia Taraya <calliataraya@Callias-MacBook-Pro-2.local>
2025-08-20 13:17:39 -0700

    wp

---
+++ practice_flow_control_and_methods/temperature_commenter.rb
@@ -0,0 +1,5 @@
+# Ask for a temperature in Fahrenheit.
+
+# < 50 → “Cold”
+# 50–80 → “Warm”
+# > 80 → “Hot”
---
+++ practice_flow_control_and_methods/yes_or_no_game.rb
@@ -0,0 +1,4 @@
+# Ask: “Do you want to play a game?”
+
+# If answer (.chomp.downcase) is "yes" or "y" → “Let’s go!”
+# Otherwise → “Maybe next time.”
