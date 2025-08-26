ae56626cbbf1700a84ff0d24e6afe35f7b99cabe
Callia Taraya <calliataraya@Callias-MacBook-Pro-3.local>
2025-08-26 10:45:11 -0700

    solve name repeater problem

--- practice_flow_control_and_methods/name_repeater.rb
+++ practice_flow_control_and_methods/name_repeater.rb
@@ -1,2 +1,17 @@
 # Ask for a name and a number.
 # Print the name that many times (one per line).
+linewidth = 75
+count = 0
+
+puts (('what is your name').center(linewidth))
+name = gets.chomp
+puts (('give me a number').center(linewidth))
+number = gets.chomp.to_i
+
+loop do
+  puts ((name).center(linewidth))
+  count += 1
+  if count == number
+    break
+  end
+end
