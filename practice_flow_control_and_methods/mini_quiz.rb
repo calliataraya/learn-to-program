c1b53eca94610e51ba31370fcc3d8df791ca8f7e
Callia Taraya <calliataraya@Callias-MacBook-Pro-3.local>
2025-08-27 10:27:08 -0700

    wp

--- practice_flow_control_and_methods/mini_quiz.rb
+++ practice_flow_control_and_methods/mini_quiz.rb
@@ -1,2 +1,55 @@
 # Ask 3 questions. For each, check if the answer is correct.
 # At the end, show how many were correct.
+
+puts "THIS IS A TEST!! yes or no answers only"
+
+puts "Is the Universe endless?"
+answer_one = gets.chomp.downcase.to_s
+
+puts "Do sound waves travel through space?"
+answer_two = gets.chomp.downcase.to_s
+
+puts "Have earth-based space crafts visited every planet in our solar system?"
+answer_third = gets.chomp.downcase.to_s
+
+if answer_one == "yes"
+  result_one = false
+elsif answer_one == "no"
+  result_one = true
+elsif answer_one == "yes"
+  result_two = false
+elsif answer_one == "no"
+  result_two = true
+elsif answer_one == "no"
+  result_three = false
+elsif answer_one == "yes"
+  result_three = true
+end
+
+points = 0
+
+loop do
+  if result_one = true
+    points += 1
+    puts "correct"
+  else
+    puts "wrong"
+  end
+  if result_two = true
+    points += 1
+    puts "correct"
+  else
+    puts "wrong"
+  end
+  if result_three = true
+    points += 1
+    puts "correct"
+  else
+    puts "wrong"
+  end
+  if points = 3
+    break
+  end
+end
+
+puts "you recieved " + points.to_s + "/3"
