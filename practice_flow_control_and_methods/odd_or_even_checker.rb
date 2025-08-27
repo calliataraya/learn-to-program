c5ea0bcd7a82144a5b938f6cab79d68b544977c3
Callia Taraya <calliataraya@Callias-MacBook-Pro-3.local>
2025-08-27 12:47:57 -0700

    organize into method

--- practice_flow_control_and_methods/odd_or_even_checker.rb
+++ practice_flow_control_and_methods/odd_or_even_checker.rb
@@ -1,15 +1,18 @@
 # Ask the user for a number. Output whether it is even or odd.
-number = 0
-
-loop do
-  puts "give me a number"
-  number = gets.chomp
-  if number.match?(/\d*/)
-    break
-  else
-    puts "try again"
+def get_number
+  loop do
+    puts "give me a number"
+    number = gets.chomp
+    if number.match?(/^-?\d+$/)
+      return number.to_i
+      break
+    else
+      puts "try again"
+    end
   end
 end
+
+number = get_number
 
 if number.to_i % 2 == 0
   puts 'this number is even'
