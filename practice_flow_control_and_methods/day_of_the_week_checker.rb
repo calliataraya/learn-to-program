d3547e75c92d721586abe04bcc5636424a01ef1f
Callia Taraya <calliataraya@Callias-MacBook-Pro-3.local>
2025-08-27 09:52:35 -0700

    solve day of the week checker problem

--- practice_flow_control_and_methods/day_of_the_week_checker.rb
+++ practice_flow_control_and_methods/day_of_the_week_checker.rb
@@ -2,3 +2,22 @@
 
 # If "Saturday" or "Sunday" → “It’s the weekend!”
 # Otherwise → “It’s a weekday!”
+
+puts "'what day is it today?' (monday, tuesday, wednesday, thursday, friday, saturday, sunday)"
+given_day = gets.chomp.downcase.to_s
+
+weekday_array = ["monday", "tuesday", "wednesday", "thursday", "friday"]
+weekend_array = ["saturday", "sunday"]
+
+if weekend_array.include?(given_day)
+  puts "it's the weekend!"
+elsif weekday_array.include?(given_day)
+  puts "it's a weekday"
+  if given_day == "wednesday"
+    puts "it's also HUMP DAY"
+  else
+    puts " :/"
+  end
+else
+  puts "I am not aware of that day of the week"
+end
