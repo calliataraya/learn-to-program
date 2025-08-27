07d3e67036e38afdb687f6e7b83fd91f20ebe7f9
Callia Taraya <calliataraya@Callias-MacBook-Pro-3.local>
2025-08-27 10:00:27 -0700

    solve grade evaluator problem

--- practice_flow_control_and_methods/grade_evaluator.rb
+++ practice_flow_control_and_methods/grade_evaluator.rb
@@ -6,3 +6,19 @@
 # 60–69 → D
 # < 60 → F
 
+puts "give me a grade (0-100)"
+given_grade = gets.chomp.to_f
+
+if given_grade >= 90.00
+  puts "You got an A!!"
+elsif given_grade <= 89.99 && given_grade >= 80
+  puts "You got a B"
+elsif given_grade <= 79.99 && given_grade >= 70
+  puts "You got a C"
+elsif given_grade <= 69.99 && given_grade >= 60
+  puts "You got a D"
+elsif given_grade <= 59.99 && given_grade >= 0
+  puts "You got an F! >:( "
+else 
+  puts "how did you manage that?"
+end
