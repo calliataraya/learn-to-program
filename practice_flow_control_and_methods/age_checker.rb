71bda648cae49729ec82fa9766d3c8f5d457ba09
Callia Taraya <calliataraya@Callias-MacBook-Pro-3.local>
2025-08-27 13:00:26 -0700

    change to case statement

--- practice_flow_control_and_methods/age_checker.rb
+++ practice_flow_control_and_methods/age_checker.rb
@@ -1,13 +1,28 @@
 # Prompt: Ask the user how old they are. If they're under 13, say they're a kid. If under 20, say teen. if under 65, say adult. Otherwise say senior.
+# grade = "B"
 
-puts 'How old are'you?'
+# case grade
+# when "A"
+#   puts "Excellent!"
+# when "B"
+#   puts "Good job!"
+# when "C"
+#   puts "You passed."
+# else
+#   puts "Try again!"
+# end
+
+
+puts 'How old are you?'
 age = gets.chomp.to_i
-if age < 13
-  puts 'you are a kid'
-elsif age < 20
-  puts 'you are a teen'
-elsif age < 65
-  puts 'you are an adult'
+
+case age
+when 0..12
+  puts "you are a kid"
+when 13..19
+  puts "you are a teen"
+when 20..64
+  puts "you are an adult"
 else
-  puts 'you are a senior'
+  puts "you are a senior"
 end
