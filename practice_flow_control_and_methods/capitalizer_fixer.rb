ed0379df2897f78bb48319d8c9d2231d41753e54
Callia Taraya <calliataraya@Callias-MacBook-Pro-3.local>
2025-09-01 10:56:53 -0700

    fix capitalizer problem

--- practice_flow_control_and_methods/capitalizer_fixer.rb
+++ practice_flow_control_and_methods/capitalizer_fixer.rb
@@ -1,9 +1,24 @@
 # Ask for a name. Output it with the first letter capitalized.
+def has_uppercase?(first_name)
+  first_name =~ /[A-Z]/
+end
+
+def has_uppercase?(last_name)
+  last_name =~ /[A-Z]/
+end
+
 
 puts 'what is your first name?'
 first_name = gets.chomp
 puts 'what is your last name?'
 last_name = gets.chomp
 
-puts ''
-puts 'so your name is ' + first_name.capitalize! + ' ' + last_name.capitalize! + '?'
+if has_uppercase?(first_name) == 0 && has_uppercase?(last_name) == 0
+  puts 'so your name is ' + first_name.to_s + ' ' + last_name.to_s + '?'
+elsif has_uppercase?(first_name) != 0 && has_uppercase?(last_name) == 0
+  puts 'so your name is ' + first_name.to_s.capitalize! + ' ' + last_name.to_s + '?'
+elsif has_uppercase?(first_name) == 0 && has_uppercase?(last_name) != 0
+  puts 'so your name is ' + first_name.to_s + ' ' + last_name.to_s.capitalize! + '?'
+else
+  puts 'so your name is ' + first_name.to_s.capitalize! + ' ' + last_name.to_s.capitalize! + '?'
+end
