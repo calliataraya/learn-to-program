070bf5ab37f51bbbd6d874cea4bb8027e6a3dbb8
Callia Taraya <calliataraya@Callias-MacBook-Pro-3.local>
2025-09-01 11:08:45 -0700

    create method to support DRY principle

--- practice_flow_control_and_methods/divisibility_tester.rb
+++ practice_flow_control_and_methods/divisibility_tester.rb
@@ -1,13 +1,17 @@
 # Ask for a number. Tell if it is divisible by 3, 5, or both.
 
+def divisible_by?(num, divisor)
+  num % divisor == 0
+end
+
 puts 'give me a whole number'
 number = gets.chomp.to_i
 
-if (number % 3 == 0) && (number % 5 == 0)
+if divisible_by?(number, 3) && divisible_by?(number, 5)
   puts "This number is divisible by 3 and 5"
-elsif number % 3 == 0
+elsif divisible_by?(number, 3)
   puts "This number is divisible by 3"
-elsif number % 5 == 0
+elsif divisible_by?(number, 5)
   puts "This number is divisible by 5"
 else
   puts "This number is not divisible by 3 or 5"
