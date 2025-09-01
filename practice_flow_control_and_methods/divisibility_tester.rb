203f2725a2db0bbad462224e28f43ebeeebe6876
Callia Taraya <calliataraya@Callias-MacBook-Pro-3.local>
2025-09-01 11:04:18 -0700

    keep if statement flat

--- practice_flow_control_and_methods/divisibility_tester.rb
+++ practice_flow_control_and_methods/divisibility_tester.rb
@@ -4,13 +4,11 @@ puts 'give me a whole number'
 number = gets.chomp.to_i
 
 if (number % 3 == 0) && (number % 5 == 0)
-  puts 'this number is divisible by 3 and 5'
+  puts "This number is divisible by 3 and 5"
+elsif number % 3 == 0
+  puts "This number is divisible by 3"
+elsif number % 5 == 0
+  puts "This number is divisible by 5"
 else
-  if number % 3 == 0
-    puts 'this number is divisible by 3'
-  elsif number % 5 == 0
-    puts 'this number is divisible by 5'
-  else
-    puts 'this number is not divisible by 3 or 5'
-  end
+  puts "This number is not divisible by 3 or 5"
 end
