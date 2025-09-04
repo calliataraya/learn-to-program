e20621f82307fcdf37d01efc0f06208d8e5b1f6e
Callia Taraya <calliataraya@Callias-MacBook-Pro-3.local>
2025-09-03 22:25:15 -0700

    organize methods in last letter matcher problem

--- practice_flow_control_and_methods/last_letter_matcher.rb
+++ practice_flow_control_and_methods/last_letter_matcher.rb
@@ -1,11 +1,21 @@
 # Ask for two words. Say whether or not they end in the same letter.
+def check_letters(word_one, word_two)
+  if word_one.match?(/\A[a-zA-Z]+\z/) && word_two.match?(/\A[a-zA-Z]+\z/)
+    true
+  else
+    puts (('** only letters please **').center($linewidth))
+    false
+  end
+end
 
 def get_words
-  puts 'word one:'
-  word_one = gets.chomp.downcase
-  puts 'word two:'
-  word_two = gets.chomp.downcase
-  return word_one, word_two
+  loop do
+    puts 'word one:'
+    word_one = gets.chomp.downcase
+    puts 'word two:'
+    word_two = gets.chomp.downcase
+    return word_one, word_two if check_letters(word_one, word_two)
+  end
 end
 
 def identify_last_letter(word_one, word_two)
@@ -16,17 +26,17 @@ end
 
 def give_results(last_digit_one, last_digit_two)
   if last_digit_one == last_digit_two
-    puts 'both of the words end with the same letter'
+    puts (('both of the words end with the same letter').center($linewidth))
   else
-    puts 'these words do not end with the same letter'
+    puts (('these words do NOT end with the same letter').center($linewidth))
   end
 end
 
 
+linewidth = 75
+$linewidth = linewidth
+
 puts 'give me two words'
 word_one, word_two = get_words
-
-identify_last_letter(word_one, word_two)
 last_digit_one, last_digit_two = identify_last_letter(word_one, word_two)
-
 give_results(last_digit_one, last_digit_two)
