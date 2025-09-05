1a20234a278beda7b289899ea4eb58f091676570
Callia Taraya <calliataraya@Callias-MacBook-Pro-3.local>
2025-09-05 13:41:15 -0700

    wp

--- practice_flow_control_and_methods/secret_word_length_hint.rb
+++ practice_flow_control_and_methods/secret_word_length_hint.rb
@@ -1,3 +1,10 @@
 # Ask for a “secret word” (don’t show it back). Output:
 
 # “Your word has ___ letters.”
+
+puts 'give me a secret word'
+secret_word = gets.chomp
+
+count_secret_word = secret_word.count("0-100")
+puts count_secret_word
+puts "your word has #{count_secret_word} letters"
