254aad25bd4c5d6778c8d98b5b48302403202907
Callia Taraya <calliataraya@Callias-MacBook-Pro-3.local>
2025-09-06 08:11:36 -0700

    debug inverted age categories problem

--- practice_debugging_ruby_code/inverted_age_categories.rb
+++ practice_debugging_ruby_code/inverted_age_categories.rb
@@ -10,14 +10,15 @@
 #   puts "You are a kid!"
 # end
 
-11. Inverted age categories
 
 puts "How old are you?"
 
 age = gets.chomp.to_i
 
-if age < 13
+if age >= 13 && age <= 19 # if age is MORE than or equal to 13 and if age is less than or equal to 19
   puts "You are a teenager!"
+elsif age >= 20
+  puts 'You are an adult'
 else
   puts "You are a kid!"
 end
