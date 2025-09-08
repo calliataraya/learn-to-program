096f7229d455a07450a4fcbaadfbaa6d2680ecdd
Callia Taraya <calliataraya@Callias-MacBook-Pro-3.local>
2025-09-08 11:12:06 -0700

    doesn't hangle empty input problem

--- practice_debugging_ruby_code/does_not_handle_empty_input.rb
+++ practice_debugging_ruby_code/does_not_handle_empty_input.rb
@@ -10,13 +10,30 @@
 #   puts "You didn’t type anything!"
 # end
 
-19. Doesn’t handle empty input
+
+def check_letters(color)
+  if color.match?(/\A[a-zA-Z]+\z/)
+    true
+  else
+    puts (('** only letters please **').center($linewidth))
+    false
+  end
+end
+
+def get_words
+  loop do
+    color = gets.chomp
+    return color if check_letters(color)
+  end
+end
+
+linewidth = 75
+$linewidth = linewidth
 
 puts "What’s your favorite color?"
+color = get_words
 
-color = gets.chomp
-
-if color.length > 0
+if color.to_s.length > 0
   puts "Nice!"
 else
   puts "You didn’t type anything!"
