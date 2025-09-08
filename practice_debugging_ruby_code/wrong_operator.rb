f815d7df463855e185581ca0bc6222446498a42c
Callia Taraya <calliataraya@Callias-MacBook-Pro-3.local>
2025-09-08 11:29:05 -0700

    debug wrong operator problem

--- practice_debugging_ruby_code/wrong_operator.rb
+++ practice_debugging_ruby_code/wrong_operator.rb
@@ -8,12 +8,13 @@
 #   puts "You guessed it!"
 # end
 
-5. Wrong operator (= vs ==)
 
 puts "Enter a secret number:"
 
 num = gets.chomp.to_i
 
-if num = 42
+if num == 42
   puts "You guessed it!"
+else
+  puts "nope"
 end
