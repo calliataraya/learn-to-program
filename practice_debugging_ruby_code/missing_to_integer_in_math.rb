1cd8fb925e0c9cfc75995862449f880ce4bb03e6
Callia Taraya <calliataraya@Callias-MacBook-Pro-3.local>
2025-09-08 11:19:46 -0700

    debug missing .to_i in math problem

--- practice_debugging_ruby_code/missing_to_integer_in_math.rb
+++ practice_debugging_ruby_code/missing_to_integer_in_math.rb
@@ -8,12 +8,10 @@
 
 # puts "You are #{age} years old."
 
-13. Missing .to_i in math
-
 puts "Enter your birth year:"
 
 birth_year = gets.chomp
 
-age = 2025 - birth_year
+age = 2025 - birth_year.to_i
 
 puts "You are #{age} years old."
