8b35cf9306c6aa0cdd05310e7bd951cfb12ca0e5
Callia Taraya <calliataraya@Callias-MacBook-Pro-3.local>
2025-09-08 11:12:55 -0700

    debug improper indentation and missing end problem

--- practice_debugging_ruby_code/improper_indentation_and_missing_end.rb
+++ practice_debugging_ruby_code/improper_indentation_and_missing_end.rb
@@ -9,13 +9,12 @@
 # else
 # puts "Odd number!"
 
-3. Improper indentation and missing end
-
 puts "Enter a number:"
 
 num = gets.chomp.to_i
 
 if num % 2 == 0
-puts "Even number!"
+  puts "Even number!"
 else
-puts "Odd number!"
+  puts "Odd number!"
+end
