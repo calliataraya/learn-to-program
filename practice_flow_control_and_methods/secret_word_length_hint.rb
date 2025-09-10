3019f5a58843a827a88975e4ff4e401f95420cef
Callia Taraya <calliataraya@Callias-MacBook-Pro-3.local>
2025-09-10 09:53:05 -0700

    solve secred word length hint problem

--- practice_flow_control_and_methods/secret_word_length_hint.rb
+++ practice_flow_control_and_methods/secret_word_length_hint.rb
@@ -5,6 +5,6 @@
 puts 'give me a secret word'
 secret_word = gets.chomp
 
-count_secret_word = secret_word.count("0-100")
+count_secret_word = secret_word.length
 puts count_secret_word
 puts "your word has #{count_secret_word} letters"
