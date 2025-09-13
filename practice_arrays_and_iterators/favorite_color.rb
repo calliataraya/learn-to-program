b291a5edaae0990fff8b04ce07e9e26626163e1f
Callia Taraya <calliataraya@Callias-MacBook-Pro-3.local>
2025-09-12 20:15:49 -0700

    consolidate favorite color array problem into method

--- practice_arrays_and_iterators/favorite_color.rb
+++ practice_arrays_and_iterators/favorite_color.rb
@@ -3,19 +3,17 @@
 # Ask the user for their 3 favorite colors, one at a time.
 # Store them in an array and print the array.
 
-inputs = []
+def get_favorite_color
+  inputs = []
+  count = 1
+  while count <= 3
+    print "What is your ##{count} favorite color?      : "
+    favorite_color = gets.chomp
+    inputs << favorite_color.capitalize
+    count += 1
+  end
+  print "Here are your top 3 favorite colors: "
+  p inputs
+end
 
-print "What is your #1 favorite color?"
-first_favorite_color = gets.chomp
-inputs << first_favorite_color
-
-print "What is your #2 favorite color?"
-second_favorite_color = gets.chomp
-inputs << second_favorite_color
-
-print "What is your #3 favorite color?"
-third_favorite_color = gets.chomp
-inputs << third_favorite_color
-
-print "Here are your top 3 favorite colors: "
-p inputs
+get_favorite_color
