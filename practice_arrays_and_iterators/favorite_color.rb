d561f47cee8d4a6e890f7f494181e03bb51129e6
Callia Taraya <calliataraya@Callias-MacBook-Pro-3.local>
2025-09-12 20:04:16 -0700

    solve favorite color array problem

--- practice_arrays_and_iterators/favorite_color.rb
+++ practice_arrays_and_iterators/favorite_color.rb
@@ -3,3 +3,19 @@
 # Ask the user for their 3 favorite colors, one at a time.
 # Store them in an array and print the array.
 
+inputs = []
+
+print "What is your #1 favorite color?"
+first_favorite_color = gets.chomp
+inputs << first_favorite_color
+
+print "What is your #2 favorite color?"
+second_favorite_color = gets.chomp
+inputs << second_favorite_color
+
+print "What is your #3 favorite color?"
+third_favorite_color = gets.chomp
+inputs << third_favorite_color
+
+print "Here are your top 3 favorite colors: "
+p inputs
