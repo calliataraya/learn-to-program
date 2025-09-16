ab09e438daf19b6b3bf1b7b643c1f2a8fdf5fb0c
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-09-15 18:03:26 -0700

    solve reverse the list problem with arrays

--- practice_arrays_and_iterators/reverse_the_list.rb
+++ practice_arrays_and_iterators/reverse_the_list.rb
@@ -2,3 +2,22 @@
 
 # Ask for 3 numbers.
 # Print them back in reverse order.
+
+def get_numbers
+  input = []
+  count = 1
+  while count <= 3
+    print "Give me a number.     ::"
+    number = gets.chomp
+
+    next if number.empty? || number.match?(/[A-Za-z]/)
+
+    input << number
+    count += 1
+  end
+  input.reverse.each do |number|
+    puts number
+  end
+end
+
+get_numbers
