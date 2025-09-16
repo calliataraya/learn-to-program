7314d1efed1ecec52d55af9668969df392e43b60
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-09-15 17:58:18 -0700

    solve greet everyone problem with arrays

--- practice_arrays_and_iterators/greet_everyone.rb
+++ practice_arrays_and_iterators/greet_everyone.rb
@@ -3,3 +3,22 @@
 # Create an array of names.
 # Use .each to print:
 # Hello, [name]! for each name.
+
+def greet_everyone
+  input = []
+  count = 1
+  while count <= 5
+    print "What is your name?     ::"
+    name = gets.chomp
+
+    next if name.empty? || name.match?(/^\d+$/)
+
+    input << name.capitalize
+    count += 1
+  end
+  input.each do |name|
+    puts "Hello, #{name}!"
+  end
+end
+
+greet_everyone
