5139994b1b2d3b81528267f1f5beedb2dae00e1f
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-09-15 17:48:00 -0700

    solve count the words problem with arrays

--- practice_arrays_and_iterators/count_the_words.rb
+++ practice_arrays_and_iterators/count_the_words.rb
@@ -3,3 +3,20 @@
 # Ask the user to enter 5 words.
 # Store them in an array.
 # Print how many words they entered using .length.
+
+def get_five_words
+inputs = []
+  count = 1
+  while count <= 5
+    print "Enter #{count} of 5 words:   "
+    word = gets.chomp.strip
+
+    next if word.empty? || word.match?(/^\d+$/)
+
+    inputs << word.capitalize
+    count += 1
+  end
+  puts "You've entered: #{inputs.length} words"
+end
+
+get_five_words
