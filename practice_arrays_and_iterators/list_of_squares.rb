e37961a5d1dd7825d06c7a0628d56c9bd3b17270
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-09-17 16:50:21 -0700

    solve list of squares problem with arrays

--- practice_arrays_and_iterators/list_of_squares.rb
+++ practice_arrays_and_iterators/list_of_squares.rb
@@ -2,3 +2,20 @@
 
 # Create an array of numbers from 1 to 5.
 # Print the square of each number.
+
+def list_of_squares
+  input = []
+  count = 1
+  while count <= 5
+    print "Give me a number.       :"
+    number = gets.chomp
+    next if number.empty? || number.match?(/[A-Za-z]/)
+    input << number.to_i
+    count += 1
+  end
+  input.each do |num|
+    print (num * num)
+  end
+end
+
+list_of_squares
