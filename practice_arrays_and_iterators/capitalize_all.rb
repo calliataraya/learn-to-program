f4ecdcd890173dcc97f25674f644ce49157e03c2
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-09-17 16:43:06 -0700

    solve capitalize all problem with arrays

--- practice_arrays_and_iterators/capitalize_all.rb
+++ practice_arrays_and_iterators/capitalize_all.rb
@@ -2,3 +2,20 @@
 
 # Ask the user for 3 lowercase words.
 # Print each one capitalized.
+
+def capitalize_words
+  input = []
+  count = 1
+  while count <= 3
+    print "Give me a lowercase word.      :"
+    word = gets.chomp.capitalize
+
+    next if word.empty? || word.match?(/^\d+$/)
+
+    input << word
+    count += 1
+  end
+  puts "Here are your three words capitalized: #{input.join(", ")}"
+end
+
+capitalize_words
