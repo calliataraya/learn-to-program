b0ad5a2de555045784feda8e0f3f59b729f88a29
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-09-24 12:06:03 -0700

    Solve sum of numbers problem with arrays

--- practice_arrays_and_iterators/sum_of_numbers.rb
+++ practice_arrays_and_iterators/sum_of_numbers.rb
@@ -2,3 +2,17 @@
 
 # Ask the user to enter 5 numbers.
 # Add them all up and print the total.
+
+numbers = []
+count = 1
+while count <= 5
+print "Give me 5 numbers. #{count}: "
+  given_number = gets.chomp
+  # confirm the given input contains only numbers
+  next if given_number.empty? || given_number.match?(/[A-Za-z]/)
+  numbers << given_number.to_i
+  count += 1
+end
+
+sum = numbers.sum 
+puts "The sum of all those numbers = #{sum}"
