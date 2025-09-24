b93ec8299b22d48f0b34cb93bc9b6ad83ecab38c
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-09-24 13:43:53 -0700

    wp

--- practice_arrays_and_iterators/build_a_shopping_list.rb
+++ practice_arrays_and_iterators/build_a_shopping_list.rb
@@ -2,3 +2,20 @@
 
 # Let the user type 5 grocery items.
 # Add each to an array and print the list at the end.
+grocery_list = {}
+
+5.times do
+  print "Enter a grocery item: "
+  item = gets.chomp
+
+  print "Enter its price: "
+  price = gets.chomp.to_f
+
+  grocery_list[item] = price
+end
+
+grocery_list.each do |item, price|
+  puts "#{item} = #{price}"
+end
+
+total = grocery_list.sum ||
