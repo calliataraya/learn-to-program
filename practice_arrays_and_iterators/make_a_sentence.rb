b283deff14e1804f45519546ce2309aac49bc016
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-10-08 11:39:05 -0700

    solve make a sentence problem using arrays

--- practice_arrays_and_iterators/make_a_sentence.rb
+++ practice_arrays_and_iterators/make_a_sentence.rb
@@ -3,3 +3,23 @@
 # Ask for 4 random words.
 # Join them into one sentence using spaces.
 # Capitalize the first word and add a period at the end.
+
+def turn_to_sentence (sentence)
+  sentence[0] = sentence[0].upcase
+  sentence << "." unless sentence.end_with?(".")
+  puts ""
+  puts sentence
+end
+
+random_input = []
+
+puts "Give me 4 random words: "
+
+4.times do |i|
+  print ":#{i + 1}: "
+  words = gets.chomp.strip
+  random_input << words
+end
+
+sentence = random_input.join(" ")
+turn_to_sentence(sentence)
