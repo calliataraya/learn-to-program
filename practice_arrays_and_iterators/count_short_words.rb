4fbf00d6f7b3ec1d5a84c33163cbf6ed521f489f
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-10-08 11:18:15 -0700

    solve count short words problem using arrays

--- practice_arrays_and_iterators/count_short_words.rb
+++ practice_arrays_and_iterators/count_short_words.rb
@@ -2,3 +2,23 @@
 
 # Ask the user to enter 5 words.
 # Print how many of those words are 4 letters or fewer.
+
+def get_words
+  under_five = 0
+  count = 1
+
+  5.times do
+    print "Give me word ##{count}: "
+    word = gets.chomp.strip
+
+    next if word.empty? || word.match?(/^\d+$/)
+
+    count += 1
+    under_five += 1 if word.length <= 4
+  end
+
+  puts ""
+  puts "#{under_five} of the words that you entered have 4 letters or fewer."
+end
+
+get_words
