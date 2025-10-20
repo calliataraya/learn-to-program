4c747da319c66efea8934f9c0ffbb2cb55497fd9
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-10-20 11:36:36 -0700

    solve print first and last problem using arrays

--- practice_arrays_and_iterators/print_first_and_last.rb
+++ practice_arrays_and_iterators/print_first_and_last.rb
@@ -2,3 +2,14 @@
 
 # Ask for 5 numbers.
 # Print the first and last numbers in the array.
+
+number_input = []
+puts "give me 5 numbers"
+
+5.times do |i|
+  print "number #{i + 1}:"
+  number = gets.chomp
+  number_input << number
+end
+
+puts "#{number_input.first}, #{number_input.last}"
