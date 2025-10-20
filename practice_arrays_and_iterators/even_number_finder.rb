7e360427347db962c07c82d3be7ca68c5cf78c73
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-10-20 11:39:59 -0700

    solve even number finder problem with arrays

--- practice_arrays_and_iterators/even_number_finder.rb
+++ practice_arrays_and_iterators/even_number_finder.rb
@@ -2,3 +2,7 @@
 
 # Create an array of numbers from 1 to 10.
 # Print only the even numbers.
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+even_numbers = numbers.select { |n| n.even? }
+puts even_numbers
