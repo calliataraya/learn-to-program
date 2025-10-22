a538d129319b94d2377bd8f342dd55e2e0526adc
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-10-22 11:45:02 -0700

    solve combine two lists problem using arrays

--- practice_arrays_and_iterators/combine_two_lists.rb
+++ practice_arrays_and_iterators/combine_two_lists.rb
@@ -3,3 +3,41 @@
 # Ask for 3 fruits and 3 vegetables.
 # Store them in separate arrays.
 # Combine both into one and print the full list.
+
+def get_fruits(fruits)
+  3.times do |i|
+    print "Fruit #{i + 1}/3: "
+    fruit_input = gets.chomp
+    fruits << fruit_input
+  end
+end
+
+def get_veggies(veggies)
+  3.times do |i|
+    print "Veggie #{i + 1}/3: "
+    veggie_input = gets.chomp
+    veggies << veggie_input
+  end
+end
+
+def print_grocery_list(full_list)
+  puts "Heres a full Grocery List:"
+  full_list.each do |grocery|
+    puts "    - #{grocery}"
+  end
+end
+
+fruits = []
+veggies = []
+
+puts "give me 3 Fruits"
+get_fruits(fruits)
+
+puts "ok now give me 3 veggies"
+get_veggies(veggies)
+
+full_list = fruits + veggies
+
+
+puts ""
+print_grocery_list(full_list)
