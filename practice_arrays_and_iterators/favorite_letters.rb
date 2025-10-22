578e6829df0002f83d8b6baf236919f77f946f31
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-10-22 11:14:03 -0700

    solve favorite letters problem using arrays

--- practice_arrays_and_iterators/favorite_letters.rb
+++ practice_arrays_and_iterators/favorite_letters.rb
@@ -4,3 +4,13 @@
 # Split it into an array of letters.
 # Print each letter with the message:
 # You typed the letter: X
+
+
+puts "give me a word"
+word_given = gets.chomp
+
+letters = word_given.chars
+
+letters.each do |letters|
+  puts "You typed the letter: #{letters}"
+end
