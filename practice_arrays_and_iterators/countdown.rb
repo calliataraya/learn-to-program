79c11c3e76607e6eafbe516a416bae9c154a86fb
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-10-22 11:05:16 -0700

    solve countdown problem using arrays

--- practice_arrays_and_iterators/countdown.rb
+++ practice_arrays_and_iterators/countdown.rb
@@ -2,3 +2,14 @@
 
 # Create an array: [5, 4, 3, 2, 1].
 # Print each number, then at the end print “Blast off!”
+
+def countdown(countdown_numbers)
+  countdown_numbers.each do |numbers|
+    puts numbers
+  end
+  puts "Blast off!"
+end
+
+countdown_numbers = ["5", "4", "3", "2", "1"]
+
+countdown(countdown_numbers)
