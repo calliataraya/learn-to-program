4b2881ce43c96854204aea5af18a479caccf1047
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-10-22 11:30:06 -0700

    solve count specific word problem using arrays

--- practice_arrays_and_iterators/count_specific_word.rb
+++ practice_arrays_and_iterators/count_specific_word.rb
@@ -2,3 +2,13 @@
 
 # Ask the user to enter 5 words.
 # Print how many of them were "dog" (case insensitive).
+words = []
+
+puts "give me 5 words"
+5.times do |i|
+  print "word #{i + 1}: "
+  words << gets.chomp
+end
+
+dog_count = words.count("dog")
+puts "You typed 'dog' #{dog_count} many times."
