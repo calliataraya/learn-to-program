77eb2055b4fe02b9a64c4c6a4e02c7026062665d
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-10-22 10:58:08 -0700

    solve list length tester problem using arrays

--- practice_arrays_and_iterators/list_length_tester.rb
+++ practice_arrays_and_iterators/list_length_tester.rb
@@ -3,3 +3,14 @@
 # Make an array with any 3 elements.
 # Ask the user to guess how long it is.
 # Tell them if they guessed correctly or not.
+
+array = ["callia", "zara", "robert"]
+
+puts "how many words do you think I have in my array?"
+answer = gets.chomp
+
+if answer == 3
+  puts "CORRECTOMUNDO!! you got it."
+elsif
+  puts "nuh uh. try again next time."
+end
