4d62952dfd40b0e9960c080af467e4b78e9109f4
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-10-22 11:20:31 -0700

    solve replace a value problem using arrays

--- practice_arrays_and_iterators/replace_a_value.rb
+++ practice_arrays_and_iterators/replace_a_value.rb
@@ -3,3 +3,19 @@
 # Create an array [1, 2, 3, 4, 5].
 # Replace the third value with 99.
 # Print the updated array.
+
+def print_array(array)
+  array.each do |numbers|
+    puts numbers
+  end
+  puts ""
+end
+
+array = ["1", "2", "3", "4", "5"]
+print_array(array)
+
+puts "I will now change 3 to 99"
+puts ""
+array[2] = 99
+
+print_array(array)
