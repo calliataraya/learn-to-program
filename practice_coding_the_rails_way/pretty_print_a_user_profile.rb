a833e22154a58335bbdf82fa83fb258b1e6fa2c0
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-10-28 16:54:45 -0700

    solve pretty print a user profile problem the rails way

--- practice_coding_the_rails_way/pretty_print_a_user_profile.rb
+++ practice_coding_the_rails_way/pretty_print_a_user_profile.rb
@@ -5,3 +5,6 @@
 # user = { "name" => "Ava", "email" => "ava@email.com" }
 # Print a sentence like:
 # Ava can be contacted at ava@email.com.
+
+user = { "name" => "Ava", "email" => "ava@email.com" }
+print "#{user["name"]} can be contacted at #{user["email"]}."
