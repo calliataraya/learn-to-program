e4ca2672511cd521f9d08f70b15443313e241dd0
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-10-28 17:28:24 -0700

    solve basic form input problem the rails way

--- practice_coding_the_rails_way/basic_form_input.rb
+++ practice_coding_the_rails_way/basic_form_input.rb
@@ -2,3 +2,19 @@
 
 # Ask the user for email, password, and username.
 # Store it in a hash and print it as a fake signup form submission.
+
+require "io/console"
+
+print "what is your email?: "
+email = gets.chomp
+print "what is your password?: "
+password = gets.chomp
+print "what is your username?: "
+username = gets.chomp
+
+user_info = {"email" => email, "password" => password, "username" => username}
+
+user_info.each do |key, value|
+  masked_value = key == "password" ? "*" * value.length : value
+  print "#{key.capitalize}: #{masked_value}\n"
+end
