0dea8b396a149809dc5831c5e92116c94eb64224
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-10-28 17:03:38 -0700

    solve index simulation problem the rails way

--- practice_coding_the_rails_way/index_simulation.rb
+++ practice_coding_the_rails_way/index_simulation.rb
@@ -3,3 +3,13 @@
 # Make an array of 3 hashes, each with a "title" and "author" (like books).
 # Loop through and print:
 # 📚 [Title] by [Author]
+
+first_book = {"title" => "To Kill a Mockingbird", "author" => "Harper Lee"}
+second_book = {"title" => "1984", "author" => "George Orwell"}
+third_book = {"title" => "The Great Gatsby", "author" => "F. Scott Fitzgerald"}
+
+books = [first_book, second_book, third_book]
+
+books.each do |book|
+  print "📚 #{book["title"]} by #{book["author"]}\n"
+end
