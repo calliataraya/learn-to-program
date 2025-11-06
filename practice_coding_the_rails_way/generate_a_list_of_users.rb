cc3e24ef6262fe32032461ad65ba1ff0179e9618
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-11-06 10:12:52 -0800

    solve generate a list of users problem the rails way

--- practice_coding_the_rails_way/generate_a_list_of_users.rb
+++ practice_coding_the_rails_way/generate_a_list_of_users.rb
@@ -4,3 +4,19 @@
 
 # { "name" => "Alex", "email" => "a@email.com" }
 # Use .each to print each user’s info.
+
+user_info = []
+3.times do
+  print "What is your name?: "
+  name = gets.chomp
+  print "What is your email?: "
+  email = gets.chomp
+
+  user_info << {"name" => name, "email" => email}
+  print "input saved\n\n"
+end
+
+user_info.each do |user|
+  print "Name: #{user["name"]}\n"
+  print "Email: #{user["email"]}\n\n"
+end
