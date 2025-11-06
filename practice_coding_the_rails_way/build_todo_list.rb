e3ddb121a650f477ba919ae3c825102b96a45898
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-11-06 09:41:31 -0800

    solve build todo list problem the rails way

--- practice_coding_the_rails_way/build_todo_list.rb
+++ practice_coding_the_rails_way/build_todo_list.rb
@@ -5,3 +5,24 @@
 
 # [{ "task" => "Buy milk", "done" => false }, ...]
 # Print each with ✅ or ❌ depending on done.
+
+todo_list = []
+
+print "Give me three tasks.\n"
+3.times do |i|
+  print "Task #{1 + i}: "
+  task = gets.chomp
+
+  print "Is it done? (yes or no): "
+  done_input = gets.chomp.downcase
+
+  done_status = done_input == "yes"
+
+  todo_list << {"task" => task, "done" => done_status}
+end
+
+print "\nYour To-Do List:"
+todo_list.each do |item|
+  status_symbol = item["done"] ? "✅" : "❌"
+  print "\n#{status_symbol} #{item["task"]}"
+end
