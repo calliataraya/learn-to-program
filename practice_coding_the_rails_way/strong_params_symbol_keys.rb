2b508849a826f4d9df9fdcd373ab3e23537b5111
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-11-11 09:49:16 -0800

    solve strong params symbol keys the rails way

--- practice_coding_the_rails_way/strong_params_symbol_keys.rb
+++ practice_coding_the_rails_way/strong_params_symbol_keys.rb
@@ -6,3 +6,16 @@
 # Convert to symbol keys:
 
 # { name: "Kai" }
+
+# I'm changing a previous problem to adhere to the strong params symbol keys solution
+
+
+first_book = {:title => "To Kill a Mockingbird", :author => "Harper Lee"}
+second_book = {:title => "1984", :author => "George Orwell"}
+third_book = {:title => "The Great Gatsby", :author => "F. Scott Fitzgerald"}
+
+books = [first_book, second_book, third_book]
+
+books.each do |book|
+  print "📚 #{book[:title]} by #{book[:author]}\n"
+end
