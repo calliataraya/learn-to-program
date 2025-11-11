ef96a2890abff8bd52e91c3f72a4b04cfa53453a
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-11-11 09:44:45 -0800

    solve simple id generator problem the rails way

--- practice_coding_the_rails_way/simple_id_generator.rb
+++ practice_coding_the_rails_way/simple_id_generator.rb
@@ -6,3 +6,48 @@
 
 # #<User id: 1, name: "Alice">
 
+users = []
+
+3.times do |i|
+  print "Give me a users name: "
+  name = gets.chomp
+  
+  users << {:id => i + 1, :name => name }
+  i += 1
+end
+
+users.each do |user|
+  print "#<User id: #{user[:id]} Name: \"#{user[:name]}\">\n"
+end
+
+
+
+
+
+
+
+# class User
+#   attr_reader :id, :name
+
+#   def initialize(id, name)
+#     @id = id
+#     @name = name
+#   end
+
+#   # This custom inspect makes it look like a Rails model
+#   def inspect
+#     "#<User id: #{@id}, name: \"#{@name}\">"
+#   end
+# end
+
+# # ----- create users -----
+# users = []
+
+# 3.times do |i|
+#   print "Give me a user's name: "
+#   name = gets.chomp
+#   users << User.new(i + 1, name)
+# end
+
+# # ----- print them -----
+# users.each { |user| puts user.inspect }
