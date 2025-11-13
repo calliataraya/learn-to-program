ae9c3148d94a949e679b09461f49353dd373d7b4
calliataraya <calliataraya@icloud.com>
2025-11-13 20:45:08 +0000

    solve sort records alphabetically problem the rails way

--- practice_coding_the_rails_way/sort_records_alphabetically.rb
+++ practice_coding_the_rails_way/sort_records_alphabetically.rb
@@ -2,3 +2,22 @@
 
 # Make an array of hashes, each with a "title" (like blog posts).
 # Sort them alphabetically by title and print them.
+
+
+first_book = {:title => "To Kill a Mockingbird", :author => "Harper Lee"}
+second_book = {:title => "1984", :author => "George Orwell"}
+third_book = {:title => "The Great Gatsby", :author => "F. Scott Fitzgerald"}
+
+books = [first_book, second_book, third_book]
+sorted_books = books.sort_by { |book| book[:title] }
+
+print "Books Listed: \n"
+books.each do |book|
+    print "#{book[:title]} by #{book[:author]}\n"
+end
+
+print "\nHere are these books sorted alphabetically by Title: \n\n"
+
+sorted_books.each do |book|
+    print "#{book[:title]} by #{book[:author]}\n"
+end
