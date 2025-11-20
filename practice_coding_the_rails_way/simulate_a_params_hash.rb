e5c502d5467e50ff9becae66d8a7fc619daf160a
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-11-20 12:00:02 -0800

    break up code into meaningful methods that do one thing

--- practice_coding_the_rails_way/simulate_a_params_hash.rb
+++ practice_coding_the_rails_way/simulate_a_params_hash.rb
@@ -6,11 +6,33 @@
 # { "name" => "Luna", "age" => "13" }
 # Print the hash.
 
-print "what is your name? "
-name = gets.chomp
-print "what is your age? "
-age = gets.chomp
+def ask(prompt)
+  print "#{prompt}: "
+  gets.chomp
+end
 
-params = { "name" => name, "age" => age }
+def permitted_params
+  {
+    name: ask("what is your name?"),
+    age: ask("what is your age?")
+  }
+end
 
-print params
+params = permitted_params
+
+puts "\nHere’s your simulated params hash:"
+puts params
+
+
+
+
+
+
+# print "what is your name? "
+# name = gets.chomp
+# print "what is your age? "
+# age = gets.chomp
+
+# params = { "name" => name, "age" => age }
+
+# print params
