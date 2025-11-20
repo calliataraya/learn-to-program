39e63c522a2d98d89e58a6477ece01a9460a3eba
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-11-20 12:14:31 -0800

    DRY up the code with a method

--- practice_coding_the_rails_way/sort_records_alphabetically.rb
+++ practice_coding_the_rails_way/sort_records_alphabetically.rb
@@ -3,21 +3,18 @@
 # Make an array of hashes, each with a "title" (like blog posts).
 # Sort them alphabetically by title and print them.
 
+def display(book)
+   puts "#{book[:title]} by #{book[:author]}"
+end
 
-first_book = {title: "To Kill a Mockingbird", author: "Harper Lee"}
-second_book = {title: "1984", author: "George Orwell"}
-third_book = {title: "The Great Gatsby", author: "F. Scott Fitzgerald"}
+books = [
+  { title: "To Kill a Mockingbird", author: "Harper Lee" },
+  { title: "1984", author: "George Orwell" },
+  { title: "The Great Gatsby", author: "F. Scott Fitzgerald" }
+]
 
 books = [first_book, second_book, third_book]
+
 sorted_books = books.sort_by { |book| book[:title] }
-
-print "Books Listed: \n"
-books.each do |book|
-    print "#{book[:title]} by #{book[:author]}\n"
-end
-
-print "\nHere are these books sorted alphabetically by Title: \n\n"
-
-sorted_books.each do |book|
-    print "#{book[:title]} by #{book[:author]}\n"
-end
+print "\nHere are these books sorted alphabetically by Title: \n"
+sorted_books.each { |book| display(book) }
