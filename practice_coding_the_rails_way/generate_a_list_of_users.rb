337a5f29e00d125163b56f9adf2bf712dcf4c419
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-11-20 11:54:36 -0800

    break up code into meaningful methods that do one thing

--- practice_coding_the_rails_way/generate_a_list_of_users.rb
+++ practice_coding_the_rails_way/generate_a_list_of_users.rb
@@ -5,18 +5,26 @@
 # { "name" => "Alex", "email" => "a@email.com" }
 # Use .each to print each user’s info.
 
+def get_email_inputs(user_info)
+  3.times do
+    print "What is your name?: "
+    name = gets.chomp
+    print "What is your email?: "
+    email = gets.chomp
+
+    user_info << {:name => name, :email => email}
+    print "input saved\n\n"
+  end
+end
+
+def list_out_emails(user_info)
+  user_info.each do |user|
+    print "Name: #{user[:name]}\n"
+    print "Email: #{user[:email]}\n\n"
+  end
+end
+
 user_info = []
-3.times do
-  print "What is your name?: "
-  name = gets.chomp
-  print "What is your email?: "
-  email = gets.chomp
 
-  user_info << {:name => name, :email => email}
-  print "input saved\n\n"
-end
-
-user_info.each do |user|
-  print "Name: #{user[:name]}\n"
-  print "Email: #{user[:email]}\n\n"
-end
+get_email_inputs(user_info)
+list_out_emails(user_info)
