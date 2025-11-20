196c3091de61a4686e68c2997ad279b95fd51857
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-11-20 09:57:45 -0800

    solve fake routes index problem the rails way

--- practice_coding_the_rails_way/fake_routes_index.rb
+++ practice_coding_the_rails_way/fake_routes_index.rb
@@ -6,3 +6,13 @@
 # GET /posts/:id
 # POST /posts
 # DELETE /posts/:id
+def common_rest_routes(name)
+  print "GET /#{name}\n"
+  print "GET /#{name}/:id\n"
+  print "POST /#{name}\n"
+  print "DELETE /#{name}/:id\n"
+end
+
+resource = {:name => "posts"}
+
+common_rest_routes(resource[:name])
