9efde8172538dc21ccd34f5ee50e7df3c177f5bf
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-11-20 11:42:04 -0800

    only accept yes or no inputs and move helper mothods under private

--- practice_coding_the_rails_way/flash_message_simulation.rb
+++ practice_coding_the_rails_way/flash_message_simulation.rb
@@ -11,23 +11,62 @@ class SessionsController
   end
 
   def create
-    print "Was your login successful? (yes/no): "
-    answer = gets.chomp.downcase
-
-    if answer == "yes"
+    if login_successful?
       @flash[:notice] = "Logged in successfully"
     else
       @flash[:alert] = "Login failed"
     end
 
-    show_flash
+    render_flash
   end
-  
-  def show_flash
-    @flash.each do |key, message|
-      puts "#{key.capitalize}: #{message}"
+
+  private
+
+  def login_successful?
+    loop do
+      print "Was your login successful? (yes/no): "
+      input = gets.chomp.strip.downcase
+
+      return true if input == "yes"
+      return false if input == "no"
+
+      puts "Invalid input. Please type 'yes' or 'no'."
+    end
+  end
+
+  def render_flash
+    @flash.each do |type, message|
+      puts "#{type.capitalize}: #{message}"
     end
   end
 end
 
 SessionsController.new.create
+
+
+# class SessionsController
+#   def initialize
+#     @flash = {}
+#   end
+
+#   def create
+#     print "Was your login successful? (yes/no): "
+#     answer = gets.chomp.downcase
+
+#     if answer == "yes"
+#       @flash[:notice] = "Logged in successfully"
+#     else
+#       @flash[:alert] = "Login failed"
+#     end
+
+#     show_flash
+#   end
+  
+#   def show_flash
+#     @flash.each do |key, message|
+#       puts "#{key.capitalize}: #{message}"
+#     end
+#   end
+# end
+
+# SessionsController.new.create
