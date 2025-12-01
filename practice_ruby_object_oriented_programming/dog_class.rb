cebf0a321abdca79a4cf83563a7b83bda3813d35
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-12-01 15:33:19 -0800

    solve dog class problem

--- practice_ruby_object_oriented_programming/dog_class.rb
+++ practice_ruby_object_oriented_programming/dog_class.rb
@@ -3,3 +3,15 @@
 # Create a Dog class with name and breed.
 # Add a method bark that returns: "Woof! I’m [name]!".
 
+class Bark
+  def initialize(name)
+    @name = name
+  end
+
+  def introduce
+  "Woof! I'm #{@name}."
+  end
+end
+
+bark = Bark.new("Chase")
+puts bark.introduce
