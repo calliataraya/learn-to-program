09b290a5c2d220cf64e0457b938dc13ca4eaeff5
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-12-01 15:45:35 -0800

    solve book class problem

--- practice_ruby_object_oriented_programming/book_class.rb
+++ practice_ruby_object_oriented_programming/book_class.rb
@@ -2,3 +2,17 @@
 
 # Make a Book class with title and author.
 # Add a method description that returns: "[Title]" by [Author].
+
+class Book
+  def initialize(title, author)
+    @title = title
+    @author = author
+  end
+
+  def describe
+    "#{@title} by #{@author}."
+  end
+end
+
+book = Book.new("1984", "George Orwell")
+puts book.describe
