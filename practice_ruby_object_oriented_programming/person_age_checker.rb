4f291aa55331196071b3b9e9eb219584b0796b39
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-12-01 16:39:45 -0800

    Solve person age checker problem

--- practice_ruby_object_oriented_programming/person_age_checker.rb
+++ practice_ruby_object_oriented_programming/person_age_checker.rb
@@ -2,3 +2,16 @@
 
 # Create a Person class with name and age.
 # Add a method teenager? that returns true if age is between 13–19.
+
+class Person
+  def initialize(name, age)
+    @name = name
+    @age = age
+  end
+  def teenager
+    @age >= 13 && @age <= 19
+  end
+end
+
+person = Person.new("Zara", 15)
+puts person.teenager
