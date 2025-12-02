02885fb49b0dd539a3a74deb8e31a71029ea716d
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-12-01 16:01:33 -0800

    Solve rectangle class problem

--- practice_ruby_object_oriented_programming/rectangle_class.rb
+++ practice_ruby_object_oriented_programming/rectangle_class.rb
@@ -2,3 +2,18 @@
 
 # Write a Rectangle class with width and height.
 # Add methods for area and perimeter.
+
+class Rectangle
+  def initialize(width, height)
+    @width = width
+    @height = height
+  end
+  def math 
+    area = @width * @height
+    perimeter = (@width * 2) + (@height * 2)
+    puts "Area: #{area} and Perimeter: #{perimeter}"
+  end
+end
+
+rectangle = Rectangle.new(4, 6)
+puts rectangle.math
