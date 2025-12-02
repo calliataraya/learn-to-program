007d7f5bbc191c743fada5d7b090de3e5b561c3d
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-12-01 16:15:13 -0800

    Solve greeting class problem

--- practice_ruby_object_oriented_programming/greeting_class.rb
+++ practice_ruby_object_oriented_programming/greeting_class.rb
@@ -2,3 +2,17 @@
 
 # Create a Greeting class that takes a name and time of day.
 # Add a method say_hello that returns: "Good morning, Luna!"
+
+class Greeting
+  def initialize(name, time)
+    @name = name
+    @time = time
+  end
+
+  def say_hello
+    "Good #{@time}, #{@name}!"
+  end
+end
+
+greeting = Greeting.new("Callia", "morning")
+puts greeting.say_hello
