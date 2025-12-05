b793849af29c3d3137b6463e8f24a23ea7284f00
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-12-05 06:28:43 -0800

    solve user with role problem

--- practice_ruby_object_oriented_programming/user_with_role.rb
+++ practice_ruby_object_oriented_programming/user_with_role.rb
@@ -2,3 +2,28 @@
 
 # Create a User class with name, email, role.
 # Method: admin? returns true if role is "admin".
+
+class User
+  def initialize(name, email, role)
+    @name = name
+    @email = email
+    @role = role.downcase
+  end
+
+  def status
+    puts "#{name}, #{email}, #{role}"
+  end
+
+  def admin?
+    @role == "admin"
+  end
+end
+
+login = User.new("Callia", "calliataraya@icloud.com", "Admin")
+if login.admin? == true
+  puts "Welcome Admin"
+  puts "Access: Granted"
+else
+  puts "User Unauthorized"
+  puts "Access: Denied"
+end
