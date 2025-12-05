23a0b9f7b582e4e853fea6d07d7a26cbbf41f89a
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-12-05 06:43:37 -0800

    solve quiz app problem

--- practice_ruby_object_oriented_programming/quiz_app.rb
+++ practice_ruby_object_oriented_programming/quiz_app.rb
@@ -2,3 +2,41 @@
 
 # Create a Quiz class that stores questions in an array of hashes.
 # Methods: add_question, start_quiz (loop, ask, score).
+
+class Quiz
+  def initialize
+    @questions = []
+  end
+
+  def add_question(question, answer)
+    @questions << { question: question, answer: answer }
+  end
+
+  def start_quiz
+    score = 0
+
+    @questions.each do |q|
+      puts q[:question]
+      print "> "
+      user_answer = gets.chomp
+
+      if user_answer.downcase == q[:answer].downcase
+        puts "Correct!"
+        score += 1
+      else
+        puts "Wrong! The correct answer was #{q[:answer]}."
+      end
+    end
+
+    puts "Your final score: #{score}/#{@questions.length}"
+  end
+end
+
+
+quiz = Quiz.new
+quiz.add_question("what is your name?", "Callia")
+quiz.add_question("what is your age?", "20")
+quiz.add_question("what is my name?", "Robert")
+quiz.add_question("what is my age?", "100")
+
+quiz.start_quiz
