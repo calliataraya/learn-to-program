47ae46c9d2fdc894406dfc3d1beb0b5140dfac25
Callia Taraya <calliataraya@Callias-MacBook-Pro-4.local>
2025-12-05 06:18:37 -0800

    solve blog post renderer problem

--- practice_ruby_object_oriented_programming/blogpost_renderer.rb
+++ practice_ruby_object_oriented_programming/blogpost_renderer.rb
@@ -5,3 +5,23 @@
 
 # summary (first 30 chars + "...")
 # formatted_date (e.g. "Jan 1, 2025")
+
+class BlogPost
+  def initialize(title, body, created_at = Time.now)
+    @title = title
+    @body = body
+    @created_at = created_at
+  end
+
+  def summary
+    @body[0...30] + "..."
+  end
+
+  def formatted_date
+    @created_at.strftime("%b %e, %Y")
+  end
+end
+
+post = BlogPost.new("My Life", "This is a really long blog post about my day and coffee.", Time.new(2025, 1, 1))
+puts post.summary
+puts post.formatted_date
